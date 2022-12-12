d67eaba989a91122e1bf9888cf43902c9a72096c
Alejandro Salas <asalas.wd@outlook.com>
2022-12-11 21:29:04 -0500

    create components folder and add shell for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+const App = () => {
+  return(
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App;
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return(
+    <nav>
+      <h1>This is my NavBar component</h1>
+    </nav>
+  )
+}
+
+export default NavBar;
