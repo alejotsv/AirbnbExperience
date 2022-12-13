afc34b3546a0933f92bed6ce51d0e1cfce18ad19
Alejandro Salas <asalas.wd@outlook.com>
2022-12-12 19:43:57 -0500

    add hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,10 @@ const Hero = () => {
   return(
     <div className='hero-banner'>
       <img src={banner} alt='hero banner' />
+      <div className='hero-text'>
+        <h1>Online Experiences</h1>
+        <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      </div>
     </div>
   );
 }
