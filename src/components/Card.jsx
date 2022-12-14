2ccceeec36805322847ebadcb736edebfd6daef6
Alejandro Salas <asalas.wd@outlook.com>
2022-12-13 20:29:44 -0500

    add rating line

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Image12 from '../assets/image12.png';
+import image12 from '../assets/image12.png';
+import star from '../assets/star.png';
 
 const Card = () => {
   return(
     <div className='card'>
-      <img src={Image12} alt='swimmer image' />
+      <img src={image12} alt='swimmer image' />
+      <div className='rating'>
+        <h4><img src={star} alt='star' /> 5.0 <em>(6) • USA</em></h4>        
+      </div>
     </div>
   );
 }
