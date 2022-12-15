6093126b3988778f66adfdfe1f95f41d0a537fab
Alejandro Salas <asalas.wd@outlook.com>
2022-12-14 22:08:11 -0500

    import card info js object

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,31 +4,12 @@ import star from '../assets/star.png';
 import swimmer from '../assets/image12.png';
 import bike from '../assets/mountain_bike.png';
 import wedding from '../assets/wedding_photography.png';
+import card_info from '../assets/cards-info.js';
 
 const Cards = () => {
   return(
     <div className='cards'>
-      <Card 
-        img={swimmer}
-        rating='5.0'
-        votes='6'
-        description='Life lessons with Katie Zaferes'
-        price='136'
-      />
-      <Card 
-        img={wedding}
-        rating='5.0'
-        votes='30'
-        description='Learn wedding photography'
-        price='125'
-      />
-      <Card 
-        img={bike}
-        rating='4.5'
-        votes='2'
-        description='Group Mountain Biking'
-        price='50'
-      />       
+    
     </div>
   )
 }
