a810f8fc74139fc26d0709599840a0f4076c876f
Alejandro Salas <asalas.wd@outlook.com>
2022-12-14 22:04:43 -0500

    add js file with cards info

---
+++ src/assets/cards-info.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    img: 'swimmer',
+    rating:'5.0',
+    votes:'6',
+    description:'Life lessons with Katie Zaferes',
+    price:'136'
+  },
+  {
+    img: 'wedding',
+    rating:'5.0',
+    votes:'30',
+    description:'Learn wedding photography',
+    price:'125'
+  },
+  {
+    img: 'bike',
+    rating:'4.5',
+    votes:'2',
+    description:'Group Mountain Biking',
+    price:'50'
+  }    
+      
+]
