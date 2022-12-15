5b1aeac843abaa4038398e7ebc59cc56e146e39b
Alejandro Salas <asalas.wd@outlook.com>
2022-12-14 22:13:25 -0500

    start array map to return each card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,9 +7,18 @@ import wedding from '../assets/wedding_photography.png';
 import card_info from '../assets/cards-info.js';
 
 const Cards = () => {
+  const info = card_info.map( card => {
+    return(
+      <Card 
+
+      />
+    )
+  } )
+  console.log(info);
+
   return(
     <div className='cards'>
-    
+      {info}
     </div>
   )
 }
